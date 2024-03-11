f77d21403ae620764072aff0080be951082a8b65
Eric Li <eric.li.8736@gmail.com>
2024-03-10 19:44:36 -0500

    Create form for logging in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,18 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useRouter } from 'next/navigation'
+import { createClient } from "@/utils/supabase/client"
+import { useState } from "react"
+import { EyeOff, Eye } from "lucide-react"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 
 
 const formSchema = z.object({
@@ -18,6 +30,13 @@ const Page = ({
 }: {
     searchParams: { message: string };
 }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+    const router = useRouter()
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -26,20 +45,82 @@ const Page = ({
         }
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        const supabase = createClient();
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email: values.username,
+            password: values.password,
+        });
+
+        if (error) {
+            return router.push("/login?message=Could not authenticate user");
+        }
+
+        return router.push("/")
+
+
     }
 
     return (
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-            <Input {...form.register("username")} />
-            <Input {...form.register("password")} />
-            <p>{form.formState.errors.username?.message}</p>
-            <Button>
-                Log in
-            </Button>
-        </form>
-    )
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="min-w-[20rem]">
+                <FormField
+                    control={form.control}
+                    name='username'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder='Email or HQS ID' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name='password'
+                    render={({ field }) => (
+                        <FormItem className="mt-6">
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <div className='relative'>
+                                    <Input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder='Password'
+                                        {...field}
+                                    />
+                                    <div className='absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer'>
+                                        {showPassword ? (
+                                            <EyeOff
+                                                className='h-6 w-6'
+                                                onClick={togglePasswordVisibility}
+                                            />
+                                        ) : (
+                                            <Eye
+                                                className='h-6 w-6'
+                                                onClick={togglePasswordVisibility}
+                                            />
+                                        )}
+                                    </div>
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type='submit' className='w-full mt-6'>
+                    Log in
+                </Button>
+                {searchParams?.message && (
+                    <p className="text-sm font-medium text-destructive">
+                        {searchParams.message}
+                    </p>
+                )}
+            </form>
+        </Form>
+    );
 }
 
 export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 
 import { db } from "@/drizzle/db";
 import { auth, users } from "@/drizzle/schema";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
   const employees = await db.select().from(users);
   console.log(employees)
+  const supabase = createClient();
+  const user  = await supabase.auth.getUser()
+  console.log(user)
   return (
     <h1>Log in</h1>
   );
