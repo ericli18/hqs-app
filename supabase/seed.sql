52376475272655d2ffc703e914714ddce6d3c1dc
Eric Li <eric.li.8736@gmail.com>
2024-04-06 15:08:10 -0500

    Add seeding sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,7 +1,28 @@
+--Location seeding
+INSERT INTO public.locations (name)
+VALUES
+('Fremont'),
+('Austin'),
+('Germany');
 
-insert into
-public.employees (name)
-values
-('Eric Li'),
-('John Doe'),
-('Jane Doe');
+--Shift type seeding
+INSERT INTO public.shift_types (label)
+VALUES
+('Morning'),
+('Afternoon'),
+('Night');
+
+--Employees seeding
+INSERT INTO public.employees (name, email, location)
+VALUES
+('John Doe', 'john.doe@example.com', 1),
+('Jane Smith', 'jane.smith@example.com', 2),
+('Eric Li', 'eric.li.8736@gmail.com', 3);
+
+--Shifts seeding
+INSERT INTO public.shifts (start_time, end_time, shift_type, employee_id)
+VALUES
+('2024-04-01 08:00:00+00', '2024-04-01 16:00:00+00', 1, 1),
+('2024-04-02 08:00:00+00', '2024-04-02 16:00:00+00', 2, 2),
+('2024-04-03 08:00:00+00', '2024-04-03 16:00:00+00', 3, 3);
+
