e59d53c01d3fa95788d183ea8520c97773549a72
Eric Li <eric.li.8736@gmail.com>
2024-08-23 20:21:20 -0500

    Add ability to specify employee headcounts

--- scheduler/src/scheduler/model.py
+++ scheduler/src/scheduler/model.py
@@ -7,9 +7,27 @@ employees = {
     "Emma": [],
     "David": [],
     "Rebecca": [],
+    "Michael": [],
+    "Sarah": [],
+    "Daniel": [],
+    "Olivia": [],
+    "James": [],
+    "Sophie": [],
 }
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 shifts = ["Morning", "Afternoon", "Evening"]
+
+
+headcounts = {
+    "Monday": {"Morning": 2, "Afternoon": 3, "Evening": 1},
+    "Tuesday": {"Morning": 2, "Afternoon": 2, "Evening": 1},
+    "Wednesday": {"Morning": 3, "Afternoon": 3, "Evening": 2},
+    "Thursday": {"Morning": 2, "Afternoon": 2, "Evening": 1},
+    "Friday": {"Morning": 3, "Afternoon": 4, "Evening": 2},
+    "Saturday": {"Morning": 3, "Afternoon": 3, "Evening": 2},
+    "Sunday": {"Morning": 2, "Afternoon": 2, "Evening": 1},
+}
+
 
 schedule = {
     e: {
@@ -19,11 +37,15 @@ schedule = {
     for e in employees
 }
 
+for d in days:
+    for s in shifts:
+        model.add(sum(schedule[e][d][s] for e in employees) == headcounts[d][s])
+
+# They work up to one shift a day...
 for e in employees:
     for d in days:
-        model.add(sum(schedule[e][d][s] for s in shifts) == 1)
-        
-print(schedule["Phil"]["Monday"]["Morning"])
+        model.add(sum(schedule[e][d][s] for s in shifts) <= 1)
+
 
 solver = cp_model.CpSolver()
 status = solver.solve(model)
@@ -37,11 +59,7 @@ if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         for d in days:
             for s in shifts:
                 if solver.value(schedule[e][d][s]):
-                    schedule_data.append({
-                        'Employee': e,
-                        'Day': d,
-                        'Shift': s
-                    })
+                    schedule_data.append({"Employee": e, "Day": d, "Shift": s})
 
     df = pd.DataFrame(schedule_data)
 
